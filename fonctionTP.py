11c59cd4c6be8280ee92ccceb2dd5a9640dcd7d7
samueljay01 <64873983+samueljay01@users.noreply.github.com>
2020-05-07 15:52:19 +0200

    Cleaning up code

--- fonctionTP.py
+++ fonctionTP.py
@@ -1,113 +1,131 @@
 import numpy as np
 import math as ms
-import matplotlib.pyplot as plt
-import csv
-import nidaqmx as mx
 
 
-def generateNonOpt(tau=0.0002, Fexc=20e+3, E0=5, Nech=1000):
-    # Function that returns the non optimizied signals 
-    
+def generate_signals(tau=0.0002, excitation_frequency=20e+3, e0=5, number_of_samples=10e2):
+    # Function that returns the non optimized signals
+
     # Physics parameters
-    Texc = 1 / Fexc                  
-    Tacq = 20 * Texc
-    Wexc = 2 * ms.pi * Fexc
-    Wexc2 = Wexc * Wexc
-    tau2 = tau * tau
-    pulsExc = 2 * ms.pi * Fexc
+    excitation_period = 1 / excitation_frequency
+    acquisition_time = 20 * excitation_period
+    omega_excitation = 2 * ms.pi * excitation_frequency
+    omega_excitation_squared = omega_excitation * omega_excitation
+    tau_squared = tau * tau  # time constant of the rc circuit
+    excitation_angular_frequency = 2 * ms.pi * excitation_frequency
 
-    B = E0 / (1 + (pulsExc ** 2) * (tau ** 2))
-    A = -B * pulsExc * tau
+    B = e0 / (1 + (excitation_angular_frequency ** 2) * (tau ** 2))
+    A = -B * excitation_angular_frequency * tau
     K = -A
-    i = (np.linspace(-0.0005, Tacq, Nech)) # Number of points used in the simulation
 
-    Vet = np.array((i < 2 * Tacq) * (i > 0) * E0 * np.sin(Wexc * i))   # excitation signal
-    Vrt = np.array((i < 2 * Tacq) * (i > 0) * tau * B * (              # signal on the edges of the resistor
-            Wexc2 * tau2 * (Wexc * np.sin(Wexc * i) - ((1 / tau) *
-            np.exp(-i / tau))) + Wexc * np.cos(Wexc * i)))
+    i = (np.linspace(-0.0005, acquisition_time, number_of_samples))
 
-    Vct = np.array((i < 2 * Tacq) * (i > 0) * (K * np.exp(-i / tau)    # signal on the edges of the capacitor
-            + A*np.cos(Wexc * i) + B*np.sin(Wexc * i)))
+    excitation_voltage = np.array((i < 2 * acquisition_time) * (i > 0) * e0 * np.sin(omega_excitation * i))
 
-    Vcht = np.array((i < 2 * Tacq) * (i > 0) * K * np.exp(-i / tau))   # signal of the capacitor discharging 
-    
-    return Vet,Vrt,Vct,Vcht,i
+    resistor_voltage = np.array((i < 2 * acquisition_time) * (i > 0) * tau * B * (
+            omega_excitation_squared * tau_squared * (omega_excitation * np.sin(omega_excitation * i) - ((1 / tau) *
+                                                                                                         np.exp(
+                                                                                                             -i / tau))) + omega_excitation * np.cos(
+        omega_excitation * i)))
+
+    capacitor_voltage = np.array((i < 2 * acquisition_time) * (i > 0) * (K * np.exp(-i / tau)
+                                                                         + A * np.cos(
+                omega_excitation * i) + B * np.sin(omega_excitation * i)))
+
+    capacitor_discharge_voltage = np.array((i < 2 * acquisition_time) * (i > 0) * K * np.exp(-i / tau))
+
+    return excitation_voltage, resistor_voltage, capacitor_voltage, capacitor_discharge_voltage, i
 
 
-def generateOpt(tau=0.0002, E0=5, freqEch=5e-7, nbPoints=20e2):
-    # Function that returns the  optimizied signals 
-    
+def generate_optimized_signals(tau=0.0002, e0=5, sample_frequency=5e-7, number_of_samples=20e2):
+    # Function that returns the optimized signals 
+
     # Physics parameters
-    freqExc = 20e3
-    pulsExc = 2 * ms.pi * freqExc
-    periodeExc = 1 / freqExc
-    B = E0 / (1 + (pulsExc ** 2) * (tau ** 2))
-    A = -B * pulsExc * tau
-    acqTime = freqEch * nbPoints
+    excitation_frequency = 20e3
+    excitation_angular_frequency = 2 * ms.pi * excitation_frequency
+    excitation_period = 1 / excitation_frequency
 
-    tempsExc = periodeExc / 4
+    B = e0 / (1 + (excitation_angular_frequency ** 2) * (tau ** 2))
+    A = -B * excitation_angular_frequency * tau
+    acquisition_time = sample_frequency * number_of_samples
 
-    temps = np.arange(0, tempsAcq, freqEch)
+    excitation_time = excitation_period / 4
 
-    Vcpt = B * np.sin(pulsExc * tempsExc) + A * np.cos(pulsExc * tempsExc)
-    d1Vcpt = pulsExc * B * np.cos(pulsExc * tempsExc) - pulsExc * A * np.sin(pulsExc * tempsExc)
-    d2Vcpt = -pulsExc ** 2 * B * np.sin(pulsExc * tempsExc) - pulsExc ** 2 * A * np.cos(pulsExc * tempsExc)
+    time = np.arange(0, acquisition_time, sample_frequency)
 
-    alpha3 = 10 / (tempsExc ** 3) * Vcpt - 4 / (tempsExc ** 2) * d1Vcpt + 1 / (2 * tempsExc) * d2Vcpt
-    alpha4 = -15 / (tempsExc ** 4) * Vcpt + 7 / (tempsExc ** 3) * d1Vcpt - 1 / (tempsExc ** 2) * d2Vcpt
-    alpha5 = 6 / (tempsExc ** 5) * Vcpt - 3 / (tempsExc ** 4) * d1Vcpt + 1 / (2 * tempsExc ** 3) * d2Vcpt
+    excitation_voltage = B * np.sin(excitation_angular_frequency * excitation_time) + A * np.cos(
+        excitation_angular_frequency * excitation_time)
+
+    first_derivative_excitation_voltage = excitation_angular_frequency * B * np.cos(
+        excitation_angular_frequency * excitation_time) - excitation_angular_frequency * A * np.sin(
+        excitation_angular_frequency * excitation_time)
+
+    second_derivative_excitation_voltage = -excitation_angular_frequency ** 2 * B * np.sin(
+        excitation_angular_frequency * excitation_time) - excitation_angular_frequency ** 2 * A * np.cos(
+        excitation_angular_frequency * excitation_time)
+
+    alpha3 = 10 / (excitation_time ** 3) * excitation_voltage - 4 / (
+            excitation_time ** 2) * first_derivative_excitation_voltage + 1 / (2 * excitation_time) \
+             * second_derivative_excitation_voltage
+    alpha4 = -15 / (excitation_time ** 4) * excitation_voltage + 7 / (
+            excitation_time ** 3) * first_derivative_excitation_voltage - 1 / (excitation_time ** 2) \
+             * second_derivative_excitation_voltage
+    alpha5 = 6 / (excitation_time ** 5) * excitation_voltage - 3 / (
+            excitation_time ** 4) * first_derivative_excitation_voltage + 1 / (2 * excitation_time ** 3) \
+             * second_derivative_excitation_voltage
 
     beta2 = tau * 3 * alpha3
     beta3 = tau * (4 * alpha4 + alpha3)
     beta4 = tau * (5 * alpha5 + alpha4)
     beta5 = tau * alpha5
 
-    VExcOpt1 = E0 * np.sin(pulsExc * temps)
-    VExcOpt2 = beta2 * temps ** 2 + beta3 * temps ** 3 + beta4 * temps ** 4 + beta5 * temps ** 5
+    optimized_excitation_voltage_1 = e0 * np.sin(excitation_angular_frequency * time)
 
-    VExcOpt = (temps < tempsAcq) * (temps > tempsExc) * VExcOpt1 \
-              + (temps <= tempsExc) * (temps > 0) * VExcOpt2
+    optimized_excitation_voltage_2 = beta2 * time ** 2 + beta3 * time ** 3 + beta4 * time ** 4 + beta5 * time ** 5
 
-    VExcOpt = np.concatenate((VExcOpt, np.zeros(round(0.5 * len(VExcOpt)))))
-    VExcOpt = np.concatenate((np.zeros(round(0.1 * len(VExcOpt))), VExcOpt))
+    optimized_excitation_voltage = (time < acquisition_time) * (time > excitation_time) * \
+                                   optimized_excitation_voltage_1 + (time <= excitation_time) * (time > 0) * \
+                                   optimized_excitation_voltage_2
 
-    totalTime = tempsAcq*len(VExcOpt)/len(temps)
-    temps = np.linspace(0,totalTime,len(VExcOpt))
-    return VExcOpt,temps
+    optimized_excitation_voltage = np.concatenate((optimized_excitation_voltage,
+                                                   np.zeros(round(0.5 * len(optimized_excitation_voltage)))))
+
+    optimized_excitation_voltage = np.concatenate((np.zeros(round(0.1 * len(optimized_excitation_voltage))),
+                                                   optimized_excitation_voltage))
+
+    total_time = acquisition_time * len(optimized_excitation_voltage) / len(time)
+    time = np.linspace(0, total_time, len(optimized_excitation_voltage))
+    return optimized_excitation_voltage, time
 
 
-def csvWrite(filename, data, delimter=',', newline='\n'):
-    np.savetxt(filename + ".csv", data, delimiter=delimter, newline=newline)
+def csv_write(filename, data, delimiter=',', newline='\n'):
+    # Function that saves data in a csv format
+    np.savetxt(filename + ".csv", data, delimiter=delimiter, newline=newline)
 
 
-def csvRead(filename, delimter=',', skiprows=0):
-    data = np.loadtxt(filename + '.csv', delimiter=delimter, skiprows=skiprows)
+def csv_read(filename, delimiter=',', skip_rows=0):
+    # Function that reads and returns from a csv format
+    data = np.loadtxt(filename + '.csv', delimiter=delimiter, skiprows=skip_rows)
     return data
 
-def inputOutputDAQmx(outputSignal, outputPort="Dev1/ao0", outMaxVal=10, outMinVal=-10,
-            inputPort="Dev1/ai0", inMaxVal=10, inMinVal=-10):
 
+def input_output_daqmx(output_signal, output_port="Dev1/ao0", max_output_value=10, min_output_value=-10,
+                       input_port="Dev1/ai0", max_input_value=10, min_input_value=-10):
+    # Function that controls the input and outputs of a national instrument acquisition card
     import nidaqmx
 
-    with nidaqmx.Task() as task, nidaqmx.Task() as task2:
+    with nidaqmx.Task() as output_task, nidaqmx.Task() as input_task:
+        output_task.ao_channels.add_ao_voltage_chan(output_port, max_val=max_output_value, min_val=min_output_value)
+        input_task.ai_channels.add_ai_voltage_chan(input_port, max_val=max_input_value, min_val=min_input_value)
 
-        task.ao_channels.add_ao_voltage_chan(outputPort, max_val=outMaxVal, min_val=outMinVal)
-        task2.ai_channels.add_ai_voltage_chan(inputPort, max_val=inMaxVal, min_val=inMinVal)
+        output_task.timing.cfg_samp_clk_timing(1e6, samps_per_chan=output_signal.size)
 
-        task.timing.cfg_samp_clk_timing(1e6, samps_per_chan=signalOpt.size)
+        output_task.write(output_signal)
+        input_signal = input_task.read(5000)
 
-        #signalOpt = fonctions.generateOpt(2e-4)
+        output_task.start()
+        input_task.start()
 
-        task.write(outputSignal)
-        inputSignal = task2.read(5000)
+        output_task.wait_until_done()
+        input_task.wait_until_done()
 
-        task.start()
-        task2.start()
-
-        task.wait_until_done()
-        task2.wait_until_done()
-
-        return inputSignal
-
-
-
+        return input_signal
